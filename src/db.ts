296ef84366ee4be15f33152c7f47351d4d8f2094
Rupam Kairi <rupamkairi@hotmail.com>
2023-05-01 16:19:17 +0000

    db prod config

--- src/db.ts
+++ src/db.ts
@@ -1,6 +1,8 @@
 import mongoose from "mongoose";
 
-const mongoURI = "mongodb://127.0.0.1:27017/cutshort-api";
+// const mongoURI = "mongodb://127.0.0.1:27017/cutshort-api";
+const mongoURI =
+  "mongodb+srv://rupamkairi:Rupam435Kairi@cluster0.5whf3.mongodb.net/";
 
 export async function dbConnect() {
   try {
