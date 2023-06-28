c21a89d2c2cec214d021d17c8c9cf0e5c9894117
Rupam Kairi <rupamkairi@hotmail.com>
2023-06-28 19:18:29 +0530

    Powershell script

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 # /bin/sh
-MONGODBURI=mongodb+srv://rupamkairi:Rupam435Kairi@cluster0.5whf3.mongodb.net/cutshort-todo JWTSECRET=vnjA8}W/NpR/hdCx}Pxjn+Zyf8d7G: COOKIESECRET=Ej*ZFM3by2!bGzsa  npm start
+MONGODBURI= JWTSECRET=vnjA8}W/NpR/hdCx}Pxjn+Zyf8d7G: COOKIESECRET=Ej*ZFM3by2!bGzsa  npm start
--- test.sh
+++ test.sh
@@ -1,4 +1,2 @@
 # /bin/sh
-MONGODBURI=mongodb+srv://rupamkairi:Rupam435Kairi@cluster0.5whf3.mongodb.net/cutshort-todo JWTSECRET=vnjA8}W/NpR/hdCx}Pxjn+Zyf8d7G: COOKIESECRET=Ej*ZFM3by2!bGzsa  npm test
-# /bin/sh
-# MONGODBURI=mongodb://127.0.0.1:27017/cutshort-api JWTSECRET=vnjA8}W/NpR/hdCx}Pxjn+Zyf8d7G: COOKIESECRET=Ej*ZFM3by2!bGzsa  npm test
+MONGODBURI= JWTSECRET=vnjA8}W/NpR/hdCx}Pxjn+Zyf8d7G: COOKIESECRET=Ej*ZFM3by2!bGzsa  npm test
